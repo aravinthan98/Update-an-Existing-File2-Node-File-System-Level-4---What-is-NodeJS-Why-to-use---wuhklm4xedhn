2b2038d1e8d30fc1ef6d1008cbaabbe1ef14030b
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2024-01-15 12:57:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
+    await fs.writeFile(fileName,fileContent);
+    const updatedContent=await fs.readFile(fileName,"utf-8");
+    return UpdatedContent;
     
 };
 
